fb9336e3c48a628c497e97fc01341c59c8b3eee1
Henrique Franzin <henriquefranzin@gmail.com>
2021-12-01 10:08:24 -0300

    build: building application

---
+++ src/main/java/com/soccer/stats/SoccerStatsApplication.java
@@ -0,0 +1,13 @@
+package com.soccer.stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerStatsApplication.class, args);
+	}
+
+}
